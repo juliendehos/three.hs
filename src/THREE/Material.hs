4630099c5af1ce7d325e60f26a3b2b78f354c540
Julien Dehos <dehos@univ-littoral.fr>
2025-07-17 13:54:21 +0200

    materials wip

--- src/THREE/Material.hs
+++ src/THREE/Material.hs
@@ -1,9 +1,7 @@
 -----------------------------------------------------------------------------
 {-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE DerivingVia                #-}
-{-# LANGUAGE TypeApplications           #-}
 {-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 -----------------------------------------------------------------------------
 module THREE.Material
   ( -- * Types
@@ -17,13 +15,44 @@ module THREE.Material
 -----------------------------------------------------------------------------
 import           Language.Javascript.JSaddle
 -----------------------------------------------------------------------------
-import           THREE.Internal as THREE
+import           THREE.Color as THREE
+import           THREE.Constants.BlendingEquations as THREE
+import           THREE.Constants.DestinationFactors as THREE
+import           THREE.Constants.SourceFactors as THREE
 import           THREE.EventDispatcher as THREE
+import           THREE.Internal as THREE
 -----------------------------------------------------------------------------
 -- | https://threejs.org/docs/#api/en/materials/Material
 class (EventDispatcher material, ToJSVal material, MakeObject material) => Material material where
   isMaterial :: ReadOnly material Bool
   isMaterial = THREE.readonly "isMaterial"
+
+  alphaHash :: Property material Bool
+  alphaHash = property "alphaHash" 
+  alphaTest :: Property material Double
+  alphaTest = property "alphaTest" 
+  alphaToCoverage :: Property material Bool
+  alphaToCoverage = property "alphaToCoverage" 
+  blendAlpha :: Property material Double
+  blendAlpha = property "blendAlpha" 
+  blendColor :: Property material Color
+  blendColor = property "blendColor" 
+  blendDst :: Property material (Maybe DestinationFactors)
+  blendDst = optional "blendDst" 
+  blendDstAlpha :: Property material (Maybe DestinationFactors)
+  blendDstAlpha = optional "blendDstAlpha" 
+  blendEquation :: Property material BlendingEquations
+  blendEquation = property "blendEquation" 
+  blendEquationAlpha :: Property material (Maybe BlendingEquations)
+  blendEquationAlpha = optional "blendEquationAlpha" 
+  -- TODO blending
+
+  blendSrc :: Property material SourceFactors
+  blendSrc = property "blendSrc" 
+  blendSrcAlpha :: Property material SourceFactors
+  blendSrcAlpha = property "blendSrcAlpha" 
+  -- TODO clipIntersection...
+
 -----------------------------------------------------------------------------
 instance Material JSVal
 -----------------------------------------------------------------------------
